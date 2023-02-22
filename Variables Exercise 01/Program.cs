ca57731fba4a108bcaf7336c7f14af3010cc0e46
johndward01 <johndward01@gmail.com>
2023-02-22 03:36:59 -0600

    done

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Variables_Exercise_01
 {
@@ -10,6 +11,9 @@ namespace Variables_Exercise_01
             // - Once finished save, commit, and then push to GitHub!
 
             // TODO: Declare 3 constansts
+            const int FavoriteNumber = 7;
+            const double PI = 3.14;
+            const string Key = "DLSK23-112!!@#$07!^$(@*&lFDLHKOSDJ2!@#(@FY7234HR2D112!!@#$2H34";
 
             #region Types
             // string
@@ -22,10 +26,31 @@ namespace Variables_Exercise_01
 
             // TODO: Declare a variable for each of the types above
             // Make sure to use C# naming convention best practices 
+            string a;
+            char b;
+            bool c;
+            int d;
+            double e;
+            decimal f;
+
 
             // TODO: Initialize each of the variables with a value
+            a = "abc";
+            b = 'b';
+            c = true;
+            d = 10;
+            e = 1.3;
+            f = 1.3333333m;
+
 
             // TODO: Write out each of the variables to the Console 
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
         }
 
     }
